dd41deec9d83780d1ed0f6eea4227c971f7975c6
struggle857 <114335452+struggle857@users.noreply.github.com>
2022-10-28 15:19:42 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void Displayboard(char board[ROW][COL], int row, int col);
+
+
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+char IsWin(char board[ROW][COL], int row, int col);
